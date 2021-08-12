d688b1cf49fb7c44b538b12eb6c8cd5380201fa7
Jeremy Wheless <whelessjw@gmail.com>
2021-08-12 10:23:34 -0400

    add Link Container on NavBar

--- client/src/components/NavigationBar.js
+++ client/src/components/NavigationBar.js
@@ -1,11 +1,14 @@
 import React from "react";
 import { Navbar, Container, Nav } from "react-bootstrap";
+import { LinkContainer } from "react-router-bootstrap";
 
 export default function NavigationBar() {
   return (
     <Navbar bg="primary" variant="dark">
       <Container>
-        <Navbar.Brand href="#home">Easy Budget Pro</Navbar.Brand>
+        <LinkContainer to="/">
+          <Navbar.Brand href="/">Easy Budget Pro</Navbar.Brand>
+        </LinkContainer>
         <Nav className="justify-content-end">
           <Nav.Link href="#link-bank-account">Link a Bank Account</Nav.Link>
           <Nav.Link href="#log-in">Log in with Google</Nav.Link>

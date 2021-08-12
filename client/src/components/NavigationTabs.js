cd8122c55ae14229611d08ee835e89c30d053704
Jeremy Wheless <whelessjw@gmail.com>
2021-08-12 13:49:42 -0400

    fix tabs

--- client/src/components/NavigationTabs.js
+++ client/src/components/NavigationTabs.js
@@ -1,34 +1,56 @@
 import React from "react";
 import { Nav } from "react-bootstrap";
 import { LinkContainer } from "react-router-bootstrap";
+import "./NavigationTabs.css";
 
 export default function NavigationTabs(props) {
-  console.log(props.defaultActiveKey);
   return (
-    <Nav variant="tabs" defaultActiveKey={props.defaultActiveKey}>
-      <LinkContainer to="/overview">
-        <Nav.Item>
-          <Nav.Link href="/overview">Overview</Nav.Link>
-        </Nav.Item>
-      </LinkContainer>
+    <>
+      <Nav defaultActiveKey={props.defaultActiveKey}>
+        <LinkContainer
+          className="tab-link"
+          activeClassName="tab-link-active"
+          exact
+          to="/overview"
+        >
+          <Nav.Item>
+            <Nav.Link href="/overview">Overview</Nav.Link>
+          </Nav.Item>
+        </LinkContainer>
 
-      <LinkContainer to="/income">
-        <Nav.Item>
-          <Nav.Link href="/income">Income</Nav.Link>
-        </Nav.Item>
-      </LinkContainer>
+        <LinkContainer
+          className="tab-link"
+          activeClassName="tab-link-active"
+          exact
+          to="/income"
+        >
+          <Nav.Item>
+            <Nav.Link href="/income">Income</Nav.Link>
+          </Nav.Item>
+        </LinkContainer>
 
-      <LinkContainer to="/expenses">
-        <Nav.Item>
-          <Nav.Link href="/expenses">Expenses</Nav.Link>
-        </Nav.Item>
-      </LinkContainer>
+        <LinkContainer
+          className="tab-link"
+          activeClassName="tab-link-active"
+          exact
+          to="/expenses"
+        >
+          <Nav.Item>
+            <Nav.Link href="/expenses">Expenses</Nav.Link>
+          </Nav.Item>
+        </LinkContainer>
 
-      <LinkContainer to="/balances">
-        <Nav.Item>
-          <Nav.Link href="/balances">Balances</Nav.Link>
-        </Nav.Item>
-      </LinkContainer>
-    </Nav>
+        <LinkContainer
+          className="tab-link"
+          activeClassName="tab-link-active"
+          exact
+          to="/balances"
+        >
+          <Nav.Item>
+            <Nav.Link href="/balances">Balances</Nav.Link>
+          </Nav.Item>
+        </LinkContainer>
+      </Nav>
+    </>
   );
 }

ec71aea351cfe37327d178cd00434af6828b74e8
Jeremy Wheless <whelessjw@gmail.com>
2021-08-31 10:10:32 -0400

    remove unused variable

--- client/src/components/OverviewTab/CategoryRow.js
+++ client/src/components/OverviewTab/CategoryRow.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { useSelector, useDispatch } from "react-redux";
+import { useDispatch } from "react-redux";
 import Button from "react-bootstrap/Button";
 import Modal from "react-bootstrap/Modal";
 import { useState } from "react";

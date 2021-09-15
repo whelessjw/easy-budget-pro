d92cb6bf629d6a170d5e15d36134a53040d3e2e2
Jeremy Wheless <whelessjw@gmail.com>
2021-09-14 20:01:57 -0400

    balances table

--- client/src/components/AccountBalancesTab/AccountBalancesContainer.js
+++ client/src/components/AccountBalancesTab/AccountBalancesContainer.js
@@ -2,17 +2,41 @@ import React from "react";
 import NavigationTabs from "../Navigation/NavigationTabs";
 import { useDispatch, useSelector } from "react-redux";
 import { getBalances } from "../../actions/index";
+import BalancesTable from "./BalancesTable";
+import PlaidLinkButton from "../Plaid/PlaidLinkButton";
 
 export default function AccountBalancesContainer() {
   const dispatch = useDispatch();
   const accessToken = useSelector((state) => state.user.plaidAccessToken);
+  const primaryAccountId = useSelector(
+    (state) => state.user.bankAccountInfo.account_id
+  );
 
   return (
     <div>
       <NavigationTabs defaultActiveKey="/balances" />
-      <button onClick={() => dispatch(getBalances(accessToken))}>
-        Get Balances
-      </button>
+
+      {!primaryAccountId && (
+        <div className="text-center">
+          <h3>Connect a bank account to view your transactions</h3>
+          <PlaidLinkButton />
+        </div>
+      )}
+
+      {primaryAccountId && (
+        <>
+          <div className="text-center mb-4 mt-3">
+            <button
+              onClick={() => dispatch(getBalances(accessToken))}
+              type="button"
+              className="btn btn-success"
+            >
+              Get Account Balances
+            </button>
+          </div>
+          <BalancesTable />
+        </>
+      )}
     </div>
   );
 }
---
+++ client/src/components/AccountBalancesTab/BalancesTable.js
@@ -0,0 +1,60 @@
+import React from "react";
+import { Table, Container, Row, Col } from "react-bootstrap";
+import { useSelector } from "react-redux";
+
+export default function BalancesTable() {
+  const balances = useSelector((state) => state.user.balances);
+
+  return (
+    <>
+      <>
+        <Container>
+          <Row className="justify-content-md-center align-items-center text-center">
+            <Col md="auto"></Col>
+            <Col xs lg="5">
+              <Table striped bordered hover>
+                <thead>
+                  <tr>
+                    <th>Account Name</th>
+                    <th>Account Type</th>
+                    <th>Current Balance</th>
+                    <th>Available Balance</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {balances.map((b) => {
+                    return (
+                      <tr>
+                        <td>
+                          {b.name.replace(/\b\w/g, (c) => c.toUpperCase())}
+                        </td>
+                        <td>
+                          {b.type.replace(/\b\w/g, (c) => c.toUpperCase())}
+                        </td>
+                        <td>
+                          {b.balances.current &&
+                            `$${b.balances.current
+                              ?.toFixed(2)
+                              .toString()
+                              .replace(/\B(?=(\d{3})+(?!\d))/g, ",")}`}
+                        </td>
+                        <td>
+                          {b.balances.available &&
+                            `$${b.balances.available
+                              ?.toFixed(2)
+                              .toString()
+                              .replace(/\B(?=(\d{3})+(?!\d))/g, ",")}`}
+                        </td>
+                      </tr>
+                    );
+                  })}
+                </tbody>
+              </Table>
+            </Col>
+            <Col md="auto"></Col>
+          </Row>
+        </Container>
+      </>
+    </>
+  );
+}

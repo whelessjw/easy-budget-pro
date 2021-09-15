e3823c7bd581d0d5397623397b90210a2656103b
Jeremy Wheless <whelessjw@gmail.com>
2021-09-14 20:12:53 -0400

    fix balance table

--- client/src/components/AccountBalancesTab/BalancesTable.js
+++ client/src/components/AccountBalancesTab/BalancesTable.js
@@ -12,44 +12,46 @@ export default function BalancesTable() {
           <Row className="justify-content-md-center align-items-center text-center">
             <Col md="auto"></Col>
             <Col xs lg="5">
-              <Table striped bordered hover>
-                <thead>
-                  <tr>
-                    <th>Account Name</th>
-                    <th>Account Type</th>
-                    <th>Current Balance</th>
-                    <th>Available Balance</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {balances.map((b) => {
-                    return (
-                      <tr>
-                        <td>
-                          {b.name.replace(/\b\w/g, (c) => c.toUpperCase())}
-                        </td>
-                        <td>
-                          {b.type.replace(/\b\w/g, (c) => c.toUpperCase())}
-                        </td>
-                        <td>
-                          {b.balances.current &&
-                            `$${b.balances.current
-                              ?.toFixed(2)
-                              .toString()
-                              .replace(/\B(?=(\d{3})+(?!\d))/g, ",")}`}
-                        </td>
-                        <td>
-                          {b.balances.available &&
-                            `$${b.balances.available
-                              ?.toFixed(2)
-                              .toString()
-                              .replace(/\B(?=(\d{3})+(?!\d))/g, ",")}`}
-                        </td>
-                      </tr>
-                    );
-                  })}
-                </tbody>
-              </Table>
+              {balances.length > 0 && (
+                <Table striped bordered hover>
+                  <thead>
+                    <tr>
+                      <th>Account Name</th>
+                      <th>Account Type</th>
+                      <th>Current Balance</th>
+                      <th>Available Balance</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {balances.map((b) => {
+                      return (
+                        <tr>
+                          <td>
+                            {b.name.replace(/\b\w/g, (c) => c.toUpperCase())}
+                          </td>
+                          <td>
+                            {b.type.replace(/\b\w/g, (c) => c.toUpperCase())}
+                          </td>
+                          <td>
+                            {b.balances.current &&
+                              `$${b.balances.current
+                                ?.toFixed(2)
+                                .toString()
+                                .replace(/\B(?=(\d{3})+(?!\d))/g, ",")}`}
+                          </td>
+                          <td>
+                            {b.balances.available &&
+                              `$${b.balances.available
+                                ?.toFixed(2)
+                                .toString()
+                                .replace(/\B(?=(\d{3})+(?!\d))/g, ",")}`}
+                          </td>
+                        </tr>
+                      );
+                    })}
+                  </tbody>
+                </Table>
+              )}
             </Col>
             <Col md="auto"></Col>
           </Row>
